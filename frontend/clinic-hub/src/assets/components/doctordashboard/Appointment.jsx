140d3c164276acd8b87b31cd8f9380b854f2c791
mohit-soni2003 <mohitsonip1847@gmail.com>
2024-09-28 14:18:13 +0530

    few final audits before hackththon

--- frontend/clinic-hub/src/assets/components/doctordashboard/Appointment.jsx
+++ frontend/clinic-hub/src/assets/components/doctordashboard/Appointment.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
-import "./DoctorDashboard.css"
+// import "./DoctorDashboard.css"
 import "./Appointment.css"
 import temp from "../../img/temp/appointments.png"
+import {Grid} from 'react-bootstrap-icons';
+import {List} from 'react-bootstrap-icons';
+
 
 import DsshboardLinks from './individualcomponent/DashboardLinks'
 
 export default function DoctorAvailablity() {
+ 
 
-  
     return (
         <div className='dr-das grid-container'>
             <div className="das-col1">
                 <DsshboardLinks></DsshboardLinks>
             </div>
             {/* -----------Availablity Status --------- */}
-            <div className="dr-das-availablity">
-               <img src={temp} alt="" />
+            <div className="dr-das-app">
+                <div className="dr-app-head">
+                    <h1>Appointments</h1>
+                    <div>
+                        <div className="grid-type1"><Grid /></div>
+                        <div className="grid-type2"><List/></div>
+                    </div>
+                </div>
             </div>
-           
+
 
         </div>
     )
